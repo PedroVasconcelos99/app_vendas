bea4534a4fafcc79e8d2d33cd376a7e7ab799d8c
Pedro Vasconcelos <pedr720@gmail.com>
2025-02-08 18:26:12 -0300

    Criando modelo Vendedores

---
+++ app/Http/Controllers/VendedoresController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Vendedores;
+use App\Http\Requests\StoreVendedoresRequest;
+use App\Http\Requests\UpdateVendedoresRequest;
+
+class VendedoresController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreVendedoresRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Vendedores $vendedores)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Vendedores $vendedores)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateVendedoresRequest $request, Vendedores $vendedores)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Vendedores $vendedores)
+    {
+        //
+    }
+}
