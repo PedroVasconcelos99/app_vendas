0a775ea3deede45a76f68b7e15b900607a696393
Pedro Vasconcelos <pedr720@gmail.com>
2025-02-09 14:57:49 -0300

    corrigindo nomes no modelo VendaProduto

--- app/Models/VendaProduto.php
+++ app/Models/VendaProduto.php
@@ -7,8 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class VendaProduto extends Model
 {
     protected $fillable = [
-        'venda_id',
-        'produto_id',
+        'id_venda',
+        'id_produto',
         'quantidade',
         'total',
     ];
