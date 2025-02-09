a75fceeb4522400b8af5a7995b53f5cf17ec880b
Pedro Vasconcelos <pedr720@gmail.com>
2025-02-09 11:16:37 -0300

    Adicionanando modelo para vendas.

---
+++ app/Http/Controllers/VendasController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Vendas;
+use App\Http\Requests\StoreVendasRequest;
+use App\Http\Requests\UpdateVendasRequest;
+
+class VendasController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreVendasRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Vendas $vendas)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Vendas $vendas)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateVendasRequest $request, Vendas $vendas)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Vendas $vendas)
+    {
+        //
+    }
+}
