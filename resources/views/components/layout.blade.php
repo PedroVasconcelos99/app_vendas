01cdfd324f0dad4d1f1e31300c8ac3f813408b35
Pedro Vasconcelos <pedr720@gmail.com>
2025-02-10 10:51:01 -0300

    Refatorando layout: Ajustando estrutura do header.

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -8,17 +8,14 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
-    <header class="">
-        <nav>
-            <div class="flex items-center gap-4">
-               <a href="{{route('home')}}" class="">Home</a>
-               <a href="{{route('clientes.index')}}" class="">Clientes</a>
-               <a href="{{route('lojas.index')}}" class="">Lojas</a>
-               <a href="{{route('vendedores.index')}}" class="">vendedores</a>
-               <a href="{{route('produtos.index')}}" class="">Produtos</a>
-               <a href="{{route('vendas.index')}}" class="">Vendas</a>
-
-           </div>
+    <header class="h-16 bg-gray-600 text-white flex items-center justify-between px-8">
+        <nav class="flex items-center gap-10">
+               <a href="{{route('home')}}" class="{{ Route::is('home') ? 'button white' : 'hover:bg-white hover:text-black hover:py-2 hover:px-4 hover:rounded-3xl transition-all duration-300 ' }}">Home</a>
+               <a href="{{route('clientes.index')}}" class="{{ Route::is('clientes*') ? 'button white' : 'hover:bg-white hover:text-black hover:py-2 hover:px-4 hover:rounded-3xl transition-all duration-300' }}">Clientes</a>
+               <a href="{{route('lojas.index')}}" class="{{ Route::is('lojas*') ? 'button white' : 'hover:bg-white hover:text-black hover:py-2 hover:px-4 hover:rounded-3xl transition-all duration-300' }}">Lojas</a>
+               <a href="{{route('vendedores.index')}}" class="{{ Route::is('vendedores*') ? 'button white' : 'hover:bg-white hover:text-black hover:py-2 hover:px-4 hover:rounded-3xl transition-all duration-300' }}">vendedores</a>
+               <a href="{{route('produtos.index')}}" class="{{ Route::is('produtos*') ? 'button white' : 'hover:bg-white hover:text-black hover:py-2 hover:px-4 hover:rounded-3xl transition-all duration-300' }}">Produtos</a>
+               <a href="{{route('vendas.index')}}" class="{{ Route::is('vendas*') ? 'button white' : 'hover:bg-white hover:text-black hover:py-2 hover:px-4 hover:rounded-3xl transition-all duration-300' }}">Vendas</a>
         </nav>
     </header>
     <main class="py-8 mx-auto">
